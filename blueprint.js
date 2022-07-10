4e8ede5876247e97394877e2d6a3030fdf9ed503
Casey <caxchen@ucsc.com>
2022-07-09 18:43:10 -0700

    got spaceOutYs working

--- blueprint.js
+++ blueprint.js
@@ -1,3 +1,5 @@
+let stop = false;
+
 class Blueprint {
     constructor() {
         this.squares = [];
@@ -33,6 +35,26 @@ class Blueprint {
 
     }
 
+    testGenerate() {
+        this.testGenerateBody();
+    }
+    testGenerateBody() {
+        this.yVertices.push(183);
+        this.yVertices.push(200);
+        this.yVertices.push(230);
+        this.yVertices.push(250);
+        this.yVertices.push(260);
+
+        this.xVertices.push(40);
+        this.xVertices.push(50);
+        this.xVertices.push(20);
+        this.xVertices.push(100);
+        this.xVertices.push(30);
+
+        this.yVertices.sort( (a,b)=>a-b );
+        this.spaceOutYs();
+    }
+
     render() {
         translate(width/2, 0);
         beginShape();
@@ -56,36 +78,56 @@ class Blueprint {
         this.xVertices.push(noise(noiseSeed1)*width/5);
         this.yVertices.push(noise(noiseSeed2)*height);
         for (let i=0; i<vertexCount; i++) {
-            this.xVertices.push( noise(noiseSeed1)*(width/2) );
-            this.yVertices.push( noise(noiseSeed2)*height );
             noiseSeed1 += noiseRate;
             noiseSeed2 += noiseRate;
+            this.xVertices.push( noise(noiseSeed1)*(width/2) );
+            this.yVertices.push( noise(noiseSeed2)*height );
         }
         this.yVertices.sort( (a,b)=>a-b );
+        //console.log("xVertice after generateBody: ", this.xVertices);
+        //console.log("yVertices after generateBody: ", this.yVertices);
         this.spaceOutYs();
         //console.log("noiseRate: ", noiseRate);
-        //console.log("xVertices: ", this.xVertices);
-        //console.log("yVertices: ", this.yVertices);
     }
 
     spaceOutYs() {
-
-        let constant = height/20;
+        let xcopies = this.xVertices;
+        let ycopies = this.yVertices;
         let changedSomething = true;
+        let xdif, ydif;
+        let maxdif = 1.7
+        let timeOut = 0;
+        try {
         while (changedSomething) {
             changedSomething = false;
             for (let i=1; i<this.yVertices.length; i++) {
-                if ((this.yVertices[i] - this.yVertices[i-1]) < constant) {
-                    this.yVertices[i] += constant;
+                xdif = this.xVertices[i] - this.xVertices[i-1];
+                ydif = this.yVertices[i] - this.yVertices[i-1];
+                if (xdif > maxdif*ydif) { //xi is bigger than x(i-1)
+                    this.xVertices[i] /= 2;
+                    changedSomething = true;
+                } else if (xdif < -maxdif*ydif) { //x(i-1) is bigger than xi  so xdif is negative
+                    this.xVertices[i-1] /= 2;
                     changedSomething = true;
                 }
             }
-            if (this.yVertices[this.yVertices.length-1] > height) {
-                this.yVertices.pop();
-                this.xVertices.pop();
-                changedSomething = true;
-            }
+            //console.log("in for loop iteration ", timeOut, ":  ", this.xVertices);
+            timeOut++;
+            if (timeOut > 200) throw "timeout";
+
         }
+        }
+        catch (a) {
+            console.log(a);
+            console.log("x vertices");
+            console.log(xcopies);
+            console.log("y vertices");
+            console.log(ycopies);
+        };
+
+        try {if (stop) throw "stopp";}
+        catch(a) {console.log(a)};
+
     }
 
 }
--- main.js
+++ main.js
@@ -5,15 +5,19 @@ function setup() {
     createCanvas(800, 800);
     ship = new Blueprint();
     ship.generate();
+    //ship.testGenerate();
 }
 
 let test = 0;
 let testrate = 0.01;
 function draw() {
     background(0);
-    ship.generate();
+    //ship.generate();
     stroke(255);
-    //line(5, 10, 5, 10+height/20);
+    let x1 = 5;
+    let y1 = 5;
+    line(x1, y1, x1*17, y1*10);
     circle(40, 6*height/7, 40);
+    line(0, height/2, width/12, height/2);
     ship.render();
 }
